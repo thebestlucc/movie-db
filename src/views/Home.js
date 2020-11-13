19424ea2d3b9ebbc8520c3231944c12430d63126
Lucas Lopes Cardoso <lucas.cardoso@solutis.net.br>
2020-11-13 19:57:34 -0300

    Add no_image.jpg at Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { POSTER_SIZE, BACKGROP_SIZE, IMAGE_BASE_URL } from '../helpers/config';
+import noImage from '../assets/images/no_image.jpg';
 
 const Home = () => {
     return <div>Home Page</div>
